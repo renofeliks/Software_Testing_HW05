3d5953aa1aa193b5bea6fdfed255aa9b4d142794
Reno <renofeliks.lindvere@gmail.com>
2025-03-26 13:12:55 +0200

    T42

--- NDv1/src/main/java/ee/ut/cs/swt/nextdate/NextDate.java
+++ NDv1/src/main/java/ee/ut/cs/swt/nextdate/NextDate.java
@@ -100,7 +100,7 @@ public class NextDate {
 //						return "Invalid Input Date";
 				}
 				//
-				else if(day > 29) //invalid input as February will never have more than 29 days
+				else //if(day > 29) //invalid input as February will never have more than 29 days
 					return "Invalid Input Date";
 			}
 		}
